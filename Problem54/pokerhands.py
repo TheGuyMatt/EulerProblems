3c061eb16ab4ba273aee270b6ee3771e38c8ce0e
Matthew Hieb <mariomatt1000@gmail.com>
2020-04-27 19:42:54 -0700

    Added variables in loop that split string into cards corresponding to players

--- Problem54/pokerhands.py
+++ Problem54/pokerhands.py
@@ -3,10 +3,16 @@
 #the file name to find the correct
 #file.
 import os.path
-scriptpath = os.path.dirname(__file__)
-filename = os.path.join(scriptpath, "hands.txt")
+script_path = os.path.dirname(__file__)
+filename = os.path.join(script_path, "hands.txt")
 
 #Read from the poker hands file
 with open(filename, "r") as f:
     for line in f:
-        print(line)
+        #split the line into separate cards
+        cards = line.split()
+        #assign correct cards to each "player"
+        player_one = [cards[0], cards[1], cards[2], cards[3], cards[4]]
+        player_two = [cards[5], cards[6], cards[7], cards[8], cards[9]]
+        
+        break
