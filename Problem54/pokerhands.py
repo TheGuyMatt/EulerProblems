26c55e122b622b898ea1f598648d44686aacb1b3
Matthew Hieb <mariomatt1000@gmail.com>
2020-04-28 16:40:32 -0700

    Added three of a kind function

--- Problem54/pokerhands.py
+++ Problem54/pokerhands.py
@@ -16,7 +16,7 @@ face = faces.split()
 lowace = lowaces.split()
 
 #creates hand from string and returns a list of "card" objects
-def makeHand(cards='2C TS 8S 8D TD'):
+def makeHand(cards='2C 3S 8S 8D 8C'):
     hand = []
     for card in cards.split():
         f, s = card[:-1], card[-1]
@@ -26,6 +26,19 @@ def makeHand(cards='2C TS 8S 8D TD'):
     assert len(hand) == 5, f"Error: Hand must be 5 cards not {len(hand)}"
     assert len(set(hand)) == 5, f"Error: Hand must have all unique cards {cards}"
     return hand
+
+#checks if hand is a "threeofakind" rank
+def threeofakind(hand):
+    allfaces = [f for f, s in hand]
+    allftypes = set(allfaces)
+    if len(allftypes) <= 2:
+        return False
+    for f in allftypes:
+        if allfaces.count(f) == 3:
+            allftypes.remove(f)
+            return 'threeofakind', [f] + sorted(allftypes, key=lambda f: face.index(f), reverse=True)
+    else:
+        return False
 
 #checks if hand is a "twopair" rank
 def twopair(hand):
@@ -75,6 +88,6 @@ if __name__ == "__main__":
             print(makeHand(player_one_str))
             print(makeHand(player_two_str))
 
-            print(twopair(makeHand()))
+            print(threeofakind(makeHand()))
 
             break
