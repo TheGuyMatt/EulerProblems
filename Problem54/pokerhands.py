c617125ea30a59f0789f98d9c4b132866c53f8a5
Matthew Hieb <mariomatt1000@gmail.com>
2020-04-28 15:41:57 -0700

    Finished makeHand class

--- Problem54/pokerhands.py
+++ Problem54/pokerhands.py
@@ -16,13 +16,15 @@ face = faces.split()
 lowace = lowaces.split()
 
 def makeHand(cards='AH AD AC AS KS'):
+    hand = []
     for card in cards.split():
         f, s = card[:-1], card[-1]
         assert f in face, f"Error: card face '{f}' does not exist"
         assert s in suit, f"Error: card suit '{s}' does not exist"
-        print(f)
-        print(s)
-
+        hand.append(Card(f, s))
+    assert len(hand) == 5, f"Error: Hand must be 5 cards not {len(hand)}"
+    assert len(set(hand)) == 5, f"Error: Hand must have all unique cards {cards}"
+    return hand
 
 #gives full file path to poker hands file
 script_path = os.path.dirname(__file__)
@@ -42,6 +44,7 @@ if __name__ == "__main__":
             player_two_str = ''.join(player_two)
 
             #creates hand of the 'card' class
-            makeHand(player_one_str)
+            print(makeHand(player_one_str))
+            print(makeHand(player_two_str))
 
             break
