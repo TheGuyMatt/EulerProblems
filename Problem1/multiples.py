e01d30ceeb39ace57a69251380529e9b97cdecbb
TheGuyMatt <matthew.hieb@protonmail.com>
2020-06-14 11:14:57 -0700

    Fixed problem 1 solution

--- Problem1/multiples.py
+++ Problem1/multiples.py
@@ -1,9 +1,9 @@
 def findSum():
     num = 0
-    for i in range(1001):
+    for i in range(1000):
         if i % 3 == 0 or i % 5 == 0:
             num += i
-    return num + 1000
+    return num
 
 if __name__ == "__main__":
     print(findSum())
